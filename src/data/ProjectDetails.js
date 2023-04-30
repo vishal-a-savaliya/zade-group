97a33c65d91efa4eb667f3e354dafb48b04db94c
vishalsavaliya <vishandsavaliya@gmail.com>
2023-04-30 16:27:12 +0530

    added fontawsome icons

--- src/data/ProjectDetails.js
+++ src/data/ProjectDetails.js
@@ -1,5 +1,33 @@
 import z2_image from '../image/z2/8.jpeg';
 import zluxuria_image from '../image/zluxuria/1_lite.jpeg';
+import {
+    faBus,
+    faBuilding,
+    faTree,
+    faChild,
+    faGazebo,
+    faWheelchair,
+    faBanquet,
+    faShieldAlt,
+    faUmbrellaBeach,
+    faCoffee,
+    faUserTie,
+    faSkating,
+    faBook,
+    faBirthdayCake,
+    faCar,
+    faBed,
+    faHotTub,
+    faPoolTable,
+    faChess,
+    faDumbbell,
+    faSwimmingPool,
+    faGamepad,
+    faTableTennis,
+    faFilm,
+    faSolarPanel,
+    faBasketballBall
+} from '@fortawesome/free-solid-svg-icons';
 
 
 export const Projects = [
@@ -57,4 +85,38 @@ export const Projects = [
             },
         ]
     }
-]; 
+];
+
+
+export const Aminities = {
+    "zluxuria": {
+        "0": { "SVG": faBus, "name": "SCHOOL DROP UP POINT" },
+        "1": { "SVG": faBuilding, "name": "DOUBLE HEIGHT ENTRANCE PORCH" },
+        "2": { "SVG": faTree, "name": "HARDSCAPE GARDEN" },
+        "3": { "SVG": faChild, "name": "CHILDREN PLAY AREA" },
+        "4": { "SVG": faGazebo, "name": "GAZEBO" },
+        "5": { "SVG": faWheelchair, "name": "SENIOR CITIZEN HANGOUT" },
+        "6": { "SVG": faBanquet, "name": "MULTIPURPOSE / BANQUET HALL" },
+        "7": { "SVG": faShieldAlt, "name": "SECURITY CABIN" },
+        "8": { "SVG": faUmbrellaBeach, "name": "SEATING WITH UMBRELLA" },
+        "9": { "SVG": faCoffee, "name": "CAFE AREA" },
+        "10": { "SVG": faUserTie, "name": "DRIVER LOUNGE" },
+        "11": { "SVG": faSkating, "name": "SKATING RINK" },
+        "12": { "SVG": faBook, "name": "LIBRARY" },
+        "13": { "SVG": faBirthdayCake, "name": "PARTY LAWN" },
+        "14": { "SVG": faCar, "name": "COVERED PARKING" },
+        "15": { "SVG": faBed, "name": "3 GUEST BEDROOM" },
+        "16": { "SVG": faTree, "name": "SEMI COVERED SEATING AREA WITH LANDSCAPE" },
+        "17": { "SVG": faHotTub, "name": "JACUZZI" },
+        "18": { "SVG": faPoolTable, "name": "POOL TABLE" },
+        "19": { "SVG": faChess, "name": "CARD | CHESS CARROM" },
+        "20": { "SVG": faDumbbell, "name": "GYM" },
+        "21": { "SVG": faSwimmingPool, "name": "SWIMMING POOL / SPLASH POOL" },
+        "22": { "SVG": faGamepad, "name": "GAME ZONE AND CAFÉ" },
+        "23": { "SVG": faTableTennis, "name": "TABLE TENNIS" },
+        "24": { "SVG": faHotTub, "name": "STEAM ROOM" },
+        "25": { "SVG": faFilm, "name": "MINI THEATER" },
+        "26": { "SVG": faSolarPanel, "name": "SOLAR PANEL AT ROOFTOP" },
+        "27": { "SVG": faBasketballBall, "name": "MULTIPURPOSE COURT" }
+    }
+}
--- src/views/Zluxuria.js
+++ src/views/Zluxuria.js
@@ -1,17 +1,20 @@
 import { useState, useEffect, useCallback } from 'react'
 import { motion } from "framer-motion"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 import img1 from '../image/zluxuria/1_lite.jpeg'
 import img2 from '../image/zluxuria/2_lite.jpeg'
 import bg from '../image/Z_Luxuriya_bg_1.jpg'
 import bg_2 from '../image/zluxuria/bg-2.png'
+import amenities_section_bg from '../image/landing_bg2.png'
 
 
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Contact from '../components/Contact'
 
-import { Projects } from '../data/ProjectDetails'
+import { Projects, Aminities } from '../data/ProjectDetails'
 
 function Zluxuria() {
 
@@ -114,6 +117,8 @@ function Zluxuria() {
 
             <Navbar />
 
+            {/* Hero */}
+
             <div className="relative h-screen">
                 <div
                     className="bg-cover bg-center h-screen w-full absolute inset-0 bg-no-repeat bg-fixed"
@@ -151,6 +156,9 @@ function Zluxuria() {
                 </div>
             </div>
 
+
+            {/* Intro */}
+
             <div className='flex justify-center flex-col items-center bg-gray py-20 bg-center'
                 style={{
                     backgroundImage: `url(${bg_2})`,
@@ -167,7 +175,7 @@ function Zluxuria() {
                     {
                         ZL.details ? ZL.details.map((data, index) => {
 
-                            if (index == ZL.details.length - 1) {
+                            if (index === ZL.details.length - 1) {
                                 return (
                                     <div className='flex justify-start items-center mx-8'>
                                         <div className='py-5'>
@@ -197,28 +205,57 @@ function Zluxuria() {
             </div>
 
 
+            {/* details  */}
+
             <div className=''>
 
                 <div className='flex flex-row'>
+
+
                     <div className='w-[400px] bg-center bg-cover'
                         style={{
                             backgroundImage: `url(${img1})`,
                         }}
                     >
-
                     </div>
+
+
+
                     <div className='h-[700px] grow flex flex-col justify-center items-center'>
 
-                        <h3 className='text-2xl text-black font-primary'>About z luxuria</h3>
-                        <h1 className='text-3xl py-3 font-style text-center text-black'>Introducing our latest project,<br /> Luxurious Living</h1>
+                        <h3 className='text-2xl text-primary font-primary font-semibold'>About z luxuria</h3>
+                        <h1 className='text-3xl py-4 font-style text-center text-black'>Introducing our latest project<br /> Luxurious Living</h1>
                         <p className='w-[450px] text-center mx-auto font-primary py-2 text-black'>z luxuria, a luxurious residential tower that will set new standards for opulent living in Ahmedabad. This magnificent 33-story tower offers 56 units with world-class amenities and stunning architectural design.</p>
 
-                        <div>
+                        <div className='flex items-center justify-center py-3 mt-2'>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#cda24b" class="w-8 h-8">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                            </svg>
+                            <h1 className='text-xl font-style ml-3'>{ZL.status}</h1>
 
                         </div>
 
+                        <div className='flex items-center justify-center py-1'>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#cda24b" class="w-7 h-7">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                            </svg>
+
+                            <span className='text-xl font-style ml-3'>{ZL.location}</span>
+
+                        </div>
+
+                        <span className="py-2 px-6 w-fit my-5 border border-primaryHover rounded-full font-primary hover:bg-primaryHover hover:text-white hover:cursor-pointer">CONTACT</span>
 
                     </div>
+
+
+
+
+
+
                     <div className='w-[400px] bg-center bg-cover'
                         style={{
                             backgroundImage: `url(${img2})`,
@@ -226,6 +263,24 @@ function Zluxuria() {
                     >
                     </div>
                 </div>
+            </div>
+
+
+            {/* Amenities */}
+
+            <div className='bg-center bg-black px-16 py-20'
+                style={{
+                    backgroundImage: `url(${amenities_section_bg})`,
+                }}
+            >
+                <h1 className='text-5xl font-style text-white text-center'>AMENITIES</h1>
+                <p className='text-2xl font-style text-white text-center py-2'>Our never ending list of stunning amenities</p>
+                {/* <FontAwesomeIcon icon={faHeart} /> */}
+
+                <div className=''>
+
+                </div>
+
             </div>
 
             <Contact />
